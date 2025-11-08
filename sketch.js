9879156ec080e3008ffb921b90c2fa5aea02543d
Thuta180391 <132090808+Thuta180391@users.noreply.github.com>
2025-11-08 16:37:26 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup()
+{
+	createCanvas(600, 600);
+	fill(255, 255, 255);
+}
+
+function draw()
+{
+	background(0, 0, 0);
+	ellipse(mouseX, mouseY, 100, 100);
+}
+
+function mousePressed()
+{	
+	fill(255,0,0);
+}
+
+function keyPressed()
+{
+	fill(255,255,0);
+}
